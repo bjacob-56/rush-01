f8c1554fdc438f7f67ab88ee750534ad8a8045c8
bjacob-56 <bjacob56@DESKTOP-HRLC1PI.localdomain>
2021-04-12 11:10:28 +0200

    maj includes

--- includes/rush01.h
+++ includes/rush01.h
@@ -1,19 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   rush01.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smorel <smorel@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: bjacob <bjacob@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 13:15:27 by smorel            #+#    #+#             */
-/*   Updated: 2020/12/05 19:24:25 by smorel           ###   ########lyon.fr   */
+/*   Updated: 2021/04/12 11:08:21 by bjacob           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef RUSH01_H
+# define RUSH01_H
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_rush;
+{
+	int	size;
+	char	c;
+	char **map_origin;
+	char **map_modif;
+	int	max;
+	int	i_max;	// line
+	int	j_max;	// column
+} t_rush;
+
 
 int		get_next_line(int fd, char **line);
 int		glen(char *str, char c);
