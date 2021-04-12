064cbb88deb04afc72bea4e9991e059380ff844e
bjacob-56 <bjacob56@DESKTOP-HRLC1PI.localdomain>
2021-04-12 15:25:50 +0200

    fix init values

--- srcs/main.c
+++ srcs/main.c
@@ -3,11 +3,11 @@
 void	init_struct(t_rush *t)
 {
 	t->c = 0;
-	t->i_max = -1;
-	t->j_max = -1;
+	t->i_max = 0;
+	t->j_max = 0;
 	t->map_modif = NULL;
 	t->map_origin = NULL;
-	t->max = 0;
+	t->max = 1;
 	t->size = 0;
 }
 
