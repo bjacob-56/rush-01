e43921ca34412deeb82f610208cdb9dc7e5441c2
bjacob-56 <bjacob56@DESKTOP-HRLC1PI.localdomain>
2021-04-12 16:19:25 +0200

    maj test opti algo

--- srcs/algo.c
+++ srcs/algo.c
@@ -12,43 +12,79 @@
 
 #include "../includes/rush01.h"
 
-static int	check_nbr(char **map_origin, int i, int j)
+static void	check_and_manage_nbr(t_rush *rush, int i, int j)
 {
-	char	height;
-
-	height = map_origin[i][j];
-	return (i != 0 && j != 0
-		&& height == map_origin[i - 1][j]
-		&& height == map_origin[i][j - 1]
-		&& height == map_origin[i - 1][j - 1]);
-}
-
-static void	get_new_size(t_rush *rush, int i, int j)
-{
-	int	val[4];
-
-	val[1] = rush->map_modif[i - 1][j];
-	val[2] = rush->map_modif[i][j - 1];
-	val[3] = rush->map_modif[i - 1][j - 1];
-	if (val[1] <= val[2])
+	if (i != 0 && j != 0
+		&& rush->map_origin[i][j] == rush->map_origin[i - 1][j]
+		&& rush->map_origin[i][j] == rush->map_origin[i][j - 1]
+		&& rush->map_origin[i][j] == rush->map_origin[i - 1][j - 1])
 	{
-		if (val[1] <= val[3])
-			val[0] = val[1] + 1;
+		if (rush->map_modif[i - 1][j] <= rush->map_modif[i][j - 1] &&
+			rush->map_modif[i - 1][j] <= rush->map_modif[i - 1][j - 1])
+			rush->map_modif[i][j] = rush->map_modif[i - 1][j] + 1;
+		else if (rush->map_modif[i][j - 1] <= rush->map_modif[i - 1][j - 1])
+			rush->map_modif[i][j] = rush->map_modif[i][j - 1] + 1;
 		else
-			val[0] = val[3] + 1;
+			rush->map_modif[i][j] = rush->map_modif[i][j - 1] + 1;
+		if (rush->map_modif[i][j] > rush->max)
+		{
+			rush->max = rush->map_modif[i][j];
+			rush->i_max = i;
+			rush->j_max = j;
+		}
 	}
-	else if (val[2] <= val[3])
-		val[0] = val[2] + 1;
 	else
-		val[0] = val[3] + 1;
-	rush->map_modif[i][j] = val[0];
-	if (val[0] > rush->max)
-	{
-		rush->max = val[0];
-		rush->i_max = i;
-		rush->j_max = j;
-	}
+		rush->map_modif[i][j] = 1;
 }
+
+// static int	check_nbr(char **map_origin, int i, int j)
+// {
+// 	char	height;
+
+// 	height = map_origin[i][j];
+// 	return (i != 0 && j != 0
+// 		&& height == map_origin[i - 1][j]
+// 		&& height == map_origin[i][j - 1]
+// 		&& height == map_origin[i - 1][j - 1]);
+// }
+
+// static void	get_new_size(t_rush *rush, int i, int j)
+// {
+// 	int	val[3];
+
+// 	val[1] = rush->map_modif[i - 1][j];
+// 	val[2] = rush->map_modif[i][j - 1];
+// 	val[0] = rush->map_modif[i - 1][j - 1];
+// 	if (val[1] <= val[2] && val[1] <= val[0])
+// 		rush->map_modif[i][j] = val[1] + 1;
+// 	else if (val[2] <= val[0])
+// 		rush->map_modif[i][j] = val[2] + 1;
+// 	else
+// 		rush->map_modif[i][j] = val[0] + 1;
+// 	if (rush->map_modif[i][j] > rush->max)
+// 	{
+// 		rush->max = rush->map_modif[i][j];
+// 		rush->i_max = i;
+// 		rush->j_max = j;
+// 	}
+// }
+
+// static void	get_new_size(t_rush *rush, int i, int j)
+// {
+// 	if (rush->map_modif[i - 1][j] <= rush->map_modif[i][j - 1] &&
+// 		rush->map_modif[i - 1][j] <= rush->map_modif[i - 1][j - 1])
+// 		rush->map_modif[i][j] = rush->map_modif[i - 1][j] + 1;
+// 	else if (rush->map_modif[i][j - 1] <= rush->map_modif[i - 1][j - 1])
+// 		rush->map_modif[i][j] = rush->map_modif[i][j - 1] + 1;
+// 	else
+// 		rush->map_modif[i][j] = rush->map_modif[i][j - 1] + 1;
+// 	if (rush->map_modif[i][j] > rush->max)
+// 	{
+// 		rush->max = rush->map_modif[i][j];
+// 		rush->i_max = i;
+// 		rush->j_max = j;
+// 	}
+// }
 
 static	void	place_camp(t_rush *rush, char **map_o, char c)
 {
@@ -64,41 +100,6 @@ static	void	place_camp(t_rush *rush, char **map_o, char c)
 	}
 }
 
-//////////////////////////////////////
-//////////////////////////////////////
-// static char *to_up_print(char *str, int size)
-// {
-// 	int i;
-
-// 	i = -1;
-// 	while (++i < size)
-// 		str[i] = str[i] + '0';
-// 	return str;
-// }
-
-// char	**ft_strdel_2d_bis(char **t, int size, int print)
-// {
-// 	int		i;
-// 	char	c;
-
-// 	i = -1;
-// 	c = '\n';
-// 	while (++i < size)
-// 	{
-// 		if (print)
-// 		{
-// 			write(1, to_up_print(t[i], size), size);
-// 			write(1, &c, 1);
-// 		}
-// 		free(t[i]);
-// 		t[i] = NULL;
-// 	}
-// 	free(t);
-// 	return (NULL);
-// }
-//////////////////////////////////////
-//////////////////////////////////////
-
 void	algo(t_rush *rush)
 {
 	int	i;
@@ -109,12 +110,7 @@ void	algo(t_rush *rush)
 	{
 		j = -1;
 		while (++j < rush->size)
-		{
-			if (check_nbr(rush->map_origin, i, j))
-				get_new_size(rush, i, j);
-			else
-				rush->map_modif[i][j] = 1;
-		}
+			check_and_manage_nbr(rush, i, j);
 	}
 	place_camp(rush, rush->map_origin, rush->c);
 	ft_strdel_2d_char(rush->map_origin, rush->size, 1);
